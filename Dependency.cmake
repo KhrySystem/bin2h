19f03e258a386dc2c5e463dadad9a5ae4fcca432
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2024-09-21 13:46:02 -0400

    Since this is being specifically used by my projects, I'm just trying to get it to function on all systems.

--- Dependency.cmake
+++ Dependency.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(DEP_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/Dependency)
-set(DEP_PROJECTS dep_clipp dep_spdlog dep_catch)
+set(DEP_PROJECTS dep_clipp dep_spdlog)
 
 ExternalProject_Add(
     dep_clipp
@@ -29,24 +29,7 @@ ExternalProject_Add(
     PATCH_COMMAND ""
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
-        -DSPDLOG_BUILD_EXAMPLES=OFF
         -DSPDLOG_BUILD_TESTS=OFF
         -DSPDLOG_INSTALL=ON
-    TEST_COMMAND ""
-)
-
-ExternalProject_Add(
-    dep_catch
-    GIT_REPOSITORY "https://github.com/catchorg/Catch2.git"
-    GIT_TAG "v2.7.0"
-    GIT_SHALLOW 1
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-    CMAKE_COMMAND
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
-        -DCATCH_BUILD_TESTING=OFF
-        -DCATCH_INSTALL_DOCS=OFF
-        -DCATCH_INSTALL_HELPERS=OFF
     TEST_COMMAND ""
 )
